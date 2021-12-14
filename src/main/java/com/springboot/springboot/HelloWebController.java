1cfb198208b9040b440dc6957e163ff4378878eb
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-12-14 18:03:49 +0530

    add:mvc controller display hellow using web page

---
+++ src/main/java/com/springboot/springboot/HelloWebController.java
@@ -0,0 +1,18 @@
+package com.springboot.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloWebController {
+    @GetMapping("/web")
+        public String Hello(){
+            return "hello";
+        }
+    @GetMapping("/web/message")
+        public String Message(Model model){
+            model.addAttribute("message","this is custom message");
+            return "message";
+        }
+}
